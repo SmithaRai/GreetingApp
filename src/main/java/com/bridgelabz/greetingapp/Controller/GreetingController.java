f1d1ad309e0e72bc426fd12afa849d7bb0a75aab
unknown <SmithaRai.com>
2022-05-22 10:02:38 +0530

    Added : added all rest calls

--- src/main/java/com/bridgelabz/greetingapp/Controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingapp/Controller/GreetingController.java
@@ -1,6 +1,10 @@
 package com.bridgelabz.greetingapp.Controller;
 
+import com.bridgelabz.greetingapp.Service.GreetingService;
 import com.bridgelabz.greetingapp.model.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -11,8 +15,11 @@ public class GreetingController {
     private static final String template="Hello %s";
     private static AtomicLong counter=new AtomicLong();
 
+    @Autowired
+    GreetingService greetingService;
+
     @GetMapping("/get")
-    public Greeting greeting(@RequestParam(value="name",defaultValue="World") String name) {
+    public Greeting greeting(@RequestParam (value="name",defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),String.format(template, name));
     }
     @PostMapping("/post")
@@ -22,5 +29,9 @@ public class GreetingController {
     @PutMapping("/put/{counter}")
     public Greeting sayHello(@PathVariable long counter,@RequestParam (value="content") String content) {
         return new Greeting(counter,String.format(template, content));
+    }
+    @GetMapping("/getMessage")
+    public ResponseEntity<String> getMessage(){
+        return new ResponseEntity<>(greetingService.getMessage(), HttpStatus.OK);
     }
 }
